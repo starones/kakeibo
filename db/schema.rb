2f66be7a6f5167f2987ced1792d77fc2041a5568
starones <itstarone172@gmail.com>
2022-02-16 12:35:52 +0000

    [Add]variablecost_valu/model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_031854) do
+ActiveRecord::Schema.define(version: 2022_02_16_123136) do
 
   create_table "fixedcost_values", force: :cascade do |t|
     t.integer "fixedcost_id", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2022_02_16_031854) do
 
   create_table "incomes", force: :cascade do |t|
     t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "variablecost_values", force: :cascade do |t|
+    t.integer "variablecost_id", null: false
+    t.date "year_month"
+    t.integer "value"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
