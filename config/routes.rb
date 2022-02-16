58c359601f8efb412b0d3157abdc0850d6906fba
starones <itstarone172@gmail.com>
2022-02-16 14:03:25 +0000

    [Add]variablecost_values/view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  root "tops#index"
  post "income_values/new(/:name)" => "income_values#new"
  post "fixedcost_values/new(/:name)" => "fixedcost_values#new"
+ post "variablecost_values/new(/:name)" => "variablecost_values#new"
 
  resources :incomes
  resources :fixedcosts
