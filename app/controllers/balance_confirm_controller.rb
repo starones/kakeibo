7ec3b9337c80e130e66c49ef11538e89e8c187aa
starones <itstarone172@gmail.com>
2022-02-17 13:23:40 +0000

    [Add]グラフ作成

--- app/controllers/balance_confirm_controller.rb
+++ app/controllers/balance_confirm_controller.rb
@@ -2,22 +2,28 @@ class BalanceConfirmController < ApplicationController
   def top
   end
 
-  def show
-    @year_month = params[:year_month] + "-01"
+	def show
+		@year_month = params[:year_month] + "-01"
+		@year_manth = params[:year_manth] + "-01"
 		@incomes = Income.order(created_at: :asc)
 		@fixedcosts = Fixedcost.order(created_at: :asc)
 		@variablecosts = Variablecost.order(created_at: :asc)
-    #収入計算
-		@income_values =IncomeValue.where(year_manth: @year_month)
+
+		#収入計算
+		@income_values = IncomeValue.where(year_manth: @year_manth)
 		@income_value_total = cal_income_total(@income_values)
+
 		#固定費計算
 		@fixedcost_values = FixedcostValue.where(year_month: @year_month)
 		@fixedcost_value_total = cal_fixedcost_total(@fixedcost_values)
+
 		#変動費計算
 		@variablecost_values = VariablecostValue.where(year_month: @year_month)
 		@variablecost_value_total = cal_variablecost_total(@variablecost_values)
+
 		#収支差
 		@balance_difference = @income_value_total - (@fixedcost_value_total + @variablecost_value_total)
+
 	end
 
 	def show_year
@@ -33,10 +39,12 @@ class BalanceConfirmController < ApplicationController
 		#年度の収入配列を作成
 		i=0
 		total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+		gon.data_incomes = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
 		year_months.each do |year_month|
 			income_values = IncomeValue.where(year_manth: year_month)
 			if income_values.present?
 				total[i] = cal_income_total(income_values)
+				gon.data_incomes[i] = total[i]
 			end
 			i += 1
 		end
@@ -45,10 +53,12 @@ class BalanceConfirmController < ApplicationController
 		#年度の固定費配列を作成
 		i=0
 		total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+		gon.data_fixedcosts = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
 		year_months.each do |year_month|
 			fixedcost_values = FixedcostValue.where(year_month: year_month)
 			if fixedcost_values.present?
 				total[i] = cal_fixedcost_total(fixedcost_values)
+				gon.data_fixedcosts[i] = total[i]
 			end
 			i += 1
 		end
@@ -57,10 +67,12 @@ class BalanceConfirmController < ApplicationController
 		#年度の変動費配列を作成
 		i=0
 		total = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+		 gon.data_variablecosts = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
 		year_months.each do |year_month|
 			variablecost_values = VariablecostValue.where(year_month: year_month)
 			if variablecost_values.present?
 				total[i] = cal_variablecost_total(variablecost_values)
+				gon.data_variablecosts[i] = total[i]
 			end
 			i += 1
 		end
@@ -68,9 +80,11 @@ class BalanceConfirmController < ApplicationController
 
 		#年度の収支結果を計算
 		@balance_differences = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
+		gon.data_results = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
 		0.upto(11) do |i|
 			if @income_value_totals[i].present?
 				@balance_differences[i] = @income_value_totals[i] - (@fixedcost_value_totals[i] + @variablecost_value_totals[i])
+				gon.data_results[i] = @balance_differences[i]
 			end
 		end
 	end
